61f9f40f4a7598363015937107c83f6997097965
Nisarga Adhikary <git@nisarga.me>
2023-10-17 06:03:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,30 @@ reddit_user_agent = 'YOUR_REDDIT_USER_AGENT'
 subreddit_name = 'YOUR_SUBREDDIT_NAME'
 discord_webhook_url = 'YOUR_DISCORD_WEBHOOK_URL'
 
-reddit = praw.Reddit(
-    client_id=reddit_client_id,
-    client_secret=reddit_client_secret,
-    username=reddit_username,
-    password=reddit_password,
-    user_agent=reddit_user_agent,
-)
+try:
+    reddit = praw.Reddit(
+        client_id=reddit_client_id,
+        client_secret=reddit_client_secret,
+        username=reddit_username,
+        password=reddit_password,
+        user_agent=reddit_user_agent,
+    )
 
-subreddit = reddit.subreddit(subreddit_name)
+    subreddit = reddit.subreddit(subreddit_name)
 
-for message in subreddit.modmail.conversations(limit=None):
-    for message in message.messages:
-        message_text = f'From: {message.author}\nSubject: {message.subject}\n\n{message.body}'
+    for message in subreddit.modmail.conversations(limit=None):
+        for message in message.messages:
+            message_text = f'From: {message.author}\nSubject: {message.subject}\n\n{message.body}'
 
-        discord_payload = {
-            'content': message_text
-        }
+            discord_payload = {
+                'content': message_text
+            }
 
-        response = requests.post(discord_webhook_url, json=discord_payload)
+            response = requests.post(discord_webhook_url, json=discord_payload)
 
-        if response.status_code == 204:
-            print('Message sent to Discord successfully!')
-        else:
-            print(f'Failed to send message to Discord. Status code: {response.status_code}')
+            if response.status_code == 204:
+                print('Message sent to Discord successfully!')
+            else:
+                print(f'Failed to send message to Discord. Status code: {response.status_code}')
+except Exception as e:
+    print(f'An error occurred: {str(e)}')
